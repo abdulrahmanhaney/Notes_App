1c60450c8c7a6e5cfeef6171e6f1efd24a250ed5
unknown <abdulrahmanhaney@gmail.com>
2023-06-02 09:59:14 +0300

    Adding theme And Create Notes View

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:notes_app/views/home_view.dart';
+
+void main() {
+  runApp(const NotesApp());
+}
+
+class NotesApp extends StatelessWidget {
+  const NotesApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        brightness: Brightness.dark,
+        useMaterial3: true,
+      ),
+      home: const SafeArea(
+        child: NotesView(),
+      ),
+    );
+  }
+}
